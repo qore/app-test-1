83d2c5b41551ae02d57d6a76f9553e8ee0dc8e06
Bastiaan Ceuppens <bastiaan.ceuppens@devoteam.com>
2018-05-09 08:12:38 +0000

    master: test

--- src/main/java/io/openshift/booster/service/GreetingProperties.java
+++ src/main/java/io/openshift/booster/service/GreetingProperties.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("greeting")
 public class GreetingProperties {
 
-    private String message = "Hello, %s!";
+    private String message = "Hello you, %s!";
 
     public String getMessage() {
         return message;
